bcd68e9b759f3643c7e34f3f14663b208a2a9a34
ThomasRen2077 <zr86@cornell.edu>
2023-09-30 15:00:14 -0400

    wrote half of inst test case

---
+++ ece4750/sim/lab2_proc/asm/inst_addi.asm
@@ -0,0 +1,27 @@
+csrr x1, mngr2proc < 5
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+addi x2, x1, 0x0004
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+csrw proc2mngr, x2 > 9
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+nop
---
+++ ece4750/sim/lab2_proc/asm/inst_andi.asm
@@ -0,0 +1,27 @@
+csrr x1, mngr2proc < 0x0f0f0f0f
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+andi x3, x1, 0x00ff
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+csrw proc2mngr, x3 >0x0000000f
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+nop
