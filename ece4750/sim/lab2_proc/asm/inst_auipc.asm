a6fe3bbd45a25a1e5150f34d944742dc4b680f1d
Mengbai Ge <mengbaig@dhcp-vl2041-45706.eduroam.cornell.edu>
2023-09-30 15:07:07 -0400

    two new added

---
+++ ece4750/sim/lab2_proc/asm/inst_auipc.asm
@@ -0,0 +1,18 @@
+auipc x1, 0x00010                       # PC=0x200
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+csrw  proc2mngr, x1 > 0x00010200
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+nop
---
+++ ece4750/sim/lab2_proc/asm/inst_lui.asm
@@ -0,0 +1,18 @@
+lui x1, 0x0001
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+csrw proc2mngr, x1 > 0x00001000
+nop
+nop
+nop
+nop
+nop
+nop
+nop
+nop
