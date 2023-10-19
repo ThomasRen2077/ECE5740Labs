8276d60294dc09ad59b0bbffaab9fd3d8675357b
ThomasRen2077 <zr86@cornell.edu>
2023-10-18 23:47:50 -0400

    Finished Testbench and Reached 100% Coverage(Clean)!

--- ece4750/sim/lab2_proc/obj_dir/utb_ProcDpathImmGen.v/verilated_cov.d
+++
@@ -1,8 +0,0 @@
-verilated_cov.o: /home/thomasyyyy2077/verilator/include/verilated_cov.cpp \
- /home/thomasyyyy2077/verilator/include/verilatedos.h \
- /home/thomasyyyy2077/verilator/include/verilated_cov.h \
- /home/thomasyyyy2077/verilator/include/verilated.h \
- /home/thomasyyyy2077/verilator/include/verilated_config.h \
- /home/thomasyyyy2077/verilator/include/verilated_types.h \
- /home/thomasyyyy2077/verilator/include/verilated_funcs.h \
- /home/thomasyyyy2077/verilator/include/verilated_cov_key.h
