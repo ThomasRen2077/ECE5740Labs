d31da9616509db8b543642ea4483f90a73273b68
Oliver_Ge <mg2463@cornell.edu>
2023-11-03 13:36:11 -0400

    added content

--- ece4750/sim/lab3_cache/CacheBase.v
+++ ece4750/sim/lab3_cache/CacheBase.v
@@ -62,11 +62,11 @@ module lab3_cache_CacheBaseDpath
   input  logic reg_en_M1
   input  tarray_en;
   input  tarray_wen;
-  input  tarray_match;
+  output tarray_match;
   input  z6b_sel;
   input  darray_write_mux_sel;
   input  darray_wen;
-
+  input  write_en_sel;
 );
 
 localparam c_reset_vector = 32'h200; 
@@ -82,10 +82,22 @@ logic [31:0] write_data;
 assign z6b = cache_request_addr_M0 & mask;
 assign offset = cache_request_addr_M0[5:2];
 assign index = cache_request_addr_M0[10:6];
-assign tag = cache_request_addr_M0[31:11]
-reg [20:0] tag_array [0:31];
-reg [511:0] data_array [0:31];
-reg dirty_array [0:31];
+assign tag = cache_request_addr_M0[31:11];
+assign data_array_output = data_array[index];
+assign tarray_match = (tag == tag_array[index]);
+assign repl_cachereq_data = {cache_req_msg_data, cache_req_msg_data, cache_req_msg_data, cache_req_msg_data,
+cache_req_msg_data, cache_req_msg_data, cache_req_msg_data, cache_req_msg_data,
+cache_req_msg_data, cache_req_msg_data, cache_req_msg_data, cache_req_msg_data,
+cache_req_msg_data, cache_req_msg_data, cache_req_msg_data, cache_req_msg_data};
+logic [20:0] tag_array [0:31];
+logic [511:0] data_array [0:31];
+logic [15:0] write_word_enable;
+logic [15:0] mask2;
+assign mask2 = 1 << offset;  // This creates a mask with a '1' at the desired offset
+assign offset_write = data | mask;    // Bitwise OR operation
+logic dirty_array [0:31];
+logic valid_array [0:31];
+
 vc_EnResetReg#(32, c_reset_vector - 32'd4) cache_request_addr_reg_M0
   (
     .clk    (clk),
@@ -120,20 +132,64 @@ vc_EnResetReg#(32, c_reset_vector - 32'd4) cache_request_data_reg_M1
     .d      (cache_request_data_M0),
     .q      (cache_request_data_M1)
   );
-
+vc_Mux2#(32) z6b_mux
+  (
+    .in0  (cache_req_msg_addr),
+    .in1  (z6b),
+    .sel  (z6b_sel),
+    .out  (memreq_msg_addr)
+  );
 vc_Mux2#(32) darray_write_mux
   (
-    .in0  (cache_request_data_M0),
+    .in0  (repl_cachereq_data),
     .in1  (memresp_msg_data),
     .sel  (darray_write_mux_sel),
     .out  (write_data)
   );
 
+vc_Mux2#(32) word_en_mux
+  (
+    .in0  (16'b1111111111111111),
+    .in1  (offset_write),
+    .sel  (write_en_sel),
+    .out  (write_word_enable)
+  );
+
 
 endmodule
 
+module lab3_cache_CacheBaseCtrl
+(
+  input  logic         clk,
+  input  logic         reset,
+  
+  output logic        mem_req_val,                         // Request Message to Data Memory Valid Signal         
+  input  logic        mem_req_rdy,                         // Data Memory Ready Signal
+  input  logic        mem_resp_val,                        // Data Memory Response Message Valid Signal
+  output logic        mem_resp_rdy,                        // Processor Ready to Receive Data Signal
+  output logic        mem_reqp_type,                       // Data Operation Type of Memory Request Message
 
+  output logic        cache_req_val,                         // Request Message to Data cache Valid Signal         
+  input  logic        cache_req_rdy,                         // Cache Ready Signal
+  input  logic        cache_resp_val,                        // Cache Response Message Valid Signal
+  output logic        cache_resp_rdy,                        // Processor Ready to Receive Cache Signal
+  output logic        cache_reqp_type,                       // Data Operation Type of Cache Request Message
+  output  logic reg_en_M0,
+  output  logic reg_en_M1,
+  output  tarray_en,
+  output  tarray_wen,
+  input  tarray_match,
+  output  z6b_sel,
+  output  darray_write_mux_sel,
+  output  darray_wen
+);
 
+logic flush;
+logic flush_done;
+
+endmodule
+
+ 
 endmodule
 
 
