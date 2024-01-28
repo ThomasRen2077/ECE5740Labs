4310a7b832e0071d6712f4a665146ea9467ae64c
ThomasRen2077 <zr86@cornell.edu>
2024-01-28 00:02:38 -0500

    Improved part of lab3_base code

--- Project/sim/lab3_cache/CacheBaseDpath.v
+++ Project/sim/lab3_cache/CacheBaseDpath.v
@@ -83,7 +83,6 @@ module lab3_cache_CacheBaseDpath
   logic dirty_array [0:31];
   logic valid_array [0:31];
 
-
   logic [20:0] current_tag;
   logic [4:0] index_M0;
   logic [3:0] offset_M0;
@@ -143,9 +142,6 @@ module lab3_cache_CacheBaseDpath
   logic [5:0]   flush_counter;
   logic [4:0]   index_spill;
 
-
-
-
   // Updating Spill Counter and Flush Counter
   always_ff@(posedge clk) begin
     if(reset) begin
@@ -175,65 +171,16 @@ module lab3_cache_CacheBaseDpath
   assign spill_initial_addr = {{tag_array[index_spill]}, {index_spill},{6'b0}};
   assign spill_addr = spill_initial_addr + {{26{1'b0}}, {spill_counter[3:0]}, {2'b0}};
 
-
-
   // Calculate Spill Data
   logic [31:0] cache_to_mem_data;
 
-
-
   always_comb begin
-      if(spill_counter ==  5'd0) begin
-          cache_to_mem_data = data_array[index_spill][31:0];
+    cache_to_mem_data = 'x;
+    for(int i = 0; i < 16; i++) begin
+      if(spill_counter ==  i) begin
+          cache_to_mem_data = data_array[index_spill][32 * i +: 32];
       end
-      else if(spill_counter ==  5'd1) begin
-          cache_to_mem_data = data_array[index_spill][63:32];
-      end
-      else if(spill_counter ==  5'd2) begin
-          cache_to_mem_data = data_array[index_spill][95:64];
-      end
-      else if(spill_counter ==  5'd3) begin
-          cache_to_mem_data = data_array[index_spill][127:96];
-      end
-      else if(spill_counter ==  5'd4) begin
-          cache_to_mem_data = data_array[index_spill][159:128];
-      end
-      else if(spill_counter ==  5'd5) begin
-          cache_to_mem_data = data_array[index_spill][191:160];
-      end
-      else if(spill_counter ==  5'd6) begin
-          cache_to_mem_data = data_array[index_spill][223:192];
-      end
-      else if(spill_counter ==  5'd7) begin
-          cache_to_mem_data = data_array[index_spill][255:224];
-      end
-      else if(spill_counter ==  5'd8) begin
-          cache_to_mem_data = data_array[index_spill][287:256];
-      end
-      else if(spill_counter ==  5'd9) begin
-          cache_to_mem_data = data_array[index_spill][319:288];
-      end
-      else if(spill_counter ==  5'd10) begin
-          cache_to_mem_data = data_array[index_spill][351:320];
-      end
-      else if(spill_counter ==  5'd11) begin
-          cache_to_mem_data = data_array[index_spill][383:352];
-      end
-      else if(spill_counter ==  5'd12) begin
-          cache_to_mem_data = data_array[index_spill][415:384];
-      end
-      else if(spill_counter ==  5'd13) begin
-          cache_to_mem_data = data_array[index_spill][447:416];
-      end
-      else if(spill_counter ==  5'd14) begin
-          cache_to_mem_data = data_array[index_spill][479:448];
-      end
-      else if(spill_counter ==  5'd15) begin
-          cache_to_mem_data = data_array[index_spill][511:480];
-      end
-      else begin
-          cache_to_mem_data = 32'bx;
-      end
+    end
   end
 
   // Assign Spill Data to Cache_to_Memory_Request Data 
@@ -316,55 +263,10 @@ module lab3_cache_CacheBaseDpath
         refill_data <= 0;
     end
     else begin
-        refill_data <= refill_data;
-
-        if(refill_resp_counter == 5'd0 && refill_one_word_resp_received) begin
-            refill_data[31:0] <= cache_resp_msg_data; 
-        end
-        else if(refill_resp_counter == 5'd1 && refill_one_word_resp_received) begin
-            refill_data[63:32] <= cache_resp_msg_data; 
-        end
-        else if(refill_resp_counter == 5'd2 && refill_one_word_resp_received) begin
-            refill_data[95:64] <= cache_resp_msg_data; 
-        end
-        else if(refill_resp_counter == 5'd3 && refill_one_word_resp_received) begin
-            refill_data[127:96] <= cache_resp_msg_data; 
-        end
-        else if(refill_resp_counter == 5'd4 && refill_one_word_resp_received) begin
-            refill_data[159:128] <= cache_resp_msg_data; 
-        end
-        else if(refill_resp_counter == 5'd5 && refill_one_word_resp_received) begin
-            refill_data[191:160] <= cache_resp_msg_data; 
-        end
-        else if(refill_resp_counter == 5'd6 && refill_one_word_resp_received) begin
-            refill_data[223:192] <= cache_resp_msg_data; 
-        end
-        else if(refill_resp_counter == 5'd7 && refill_one_word_resp_received) begin
-            refill_data[255:224] <= cache_resp_msg_data; 
-        end
-        else if(refill_resp_counter == 5'd8 && refill_one_word_resp_received) begin
-            refill_data[287:256] <= cache_resp_msg_data; 
-        end
-        else if(refill_resp_counter == 5'd9 && refill_one_word_resp_received) begin
-            refill_data[319:288] <= cache_resp_msg_data; 
-        end
-        else if(refill_resp_counter == 5'd10 && refill_one_word_resp_received) begin
-            refill_data[351:320] <= cache_resp_msg_data; 
-        end
-        else if(refill_resp_counter == 5'd11 && refill_one_word_resp_received) begin
-            refill_data[383:352] <= cache_resp_msg_data; 
-        end
-        else if(refill_resp_counter == 5'd12 && refill_one_word_resp_received) begin
-            refill_data[415:384] <= cache_resp_msg_data; 
-        end
-        else if(refill_resp_counter == 5'd13 && refill_one_word_resp_received) begin
-            refill_data[447:416] <= cache_resp_msg_data; 
-        end
-        else if(refill_resp_counter == 5'd14 && refill_one_word_resp_received) begin
-            refill_data[479:448] <= cache_resp_msg_data; 
-        end
-        else if(refill_resp_counter == 5'd15 && refill_one_word_resp_received) begin
-            refill_data[511:480] <= cache_resp_msg_data; 
+        for(int i = 0; i < 16; i++) begin
+          if((refill_resp_counter == i) & refill_one_word_resp_received) begin
+            refill_data[32 * i +: 32] <= cache_resp_msg_data; 
+          end
         end
     end
   end
@@ -375,11 +277,9 @@ module lab3_cache_CacheBaseDpath
     else                                refill_resp_done =1'b0;
   end
 
-
 //--------------------------------------------------------------------
 // Return to PIPE
 //--------------------------------------------------------------------
-
 
   // Select Cache to Memory Request Address 
   vc_Mux2#(32) Spill_or_Refill_Mux
@@ -389,7 +289,6 @@ module lab3_cache_CacheBaseDpath
     .sel  (Spill_or_Refill_sel),
     .out  (cache_req_msg_addr)
   );
-
 
   // Walk through Write Data Select Mux
   logic [511:0] write_data;
@@ -422,9 +321,9 @@ module lab3_cache_CacheBaseDpath
   // Write data to CacheLine
   always_ff@(posedge clk) begin
     if (reset) begin
-      for (logic[5:0] i = 0; i < 32; i++) begin
-        data_array[i[4:0]] <= 0;
-        dirty_array[i[4:0]] <= 1'b0;
+      for (int i = 0; i < 32; i++) begin
+        data_array[i] <= 0;
+        dirty_array[i] <= 1'b0;
       end
     end
     else begin
@@ -436,72 +335,13 @@ module lab3_cache_CacheBaseDpath
       if(flush && spill_done)                   dirty_array[index_spill] <= 1'b0;
 
       // Write Data
-      if(write_word_enable[0] && darray_wen) begin 
-        data_array[index_M0][31:0] <= write_data[31:0];
-      end
-
-      if(write_word_enable[1] && darray_wen) begin
-        data_array[index_M0][63:32] <= write_data[63:32];
-      end
-
-      if(write_word_enable[2] && darray_wen) begin
-        data_array[index_M0][95:64] <= write_data[95:64];
-      end
-
-      if(write_word_enable[3] && darray_wen) begin
-        data_array[index_M0][127:96] <= write_data[127:96];
-      end
-
-      if(write_word_enable[4] && darray_wen) begin
-        data_array[index_M0][159:128] <= write_data[159:128];
-      end
-
-      if(write_word_enable[5] && darray_wen) begin
-        data_array[index_M0][191:160] <= write_data[191:160];
-      end
-
-      if(write_word_enable[6] && darray_wen) begin
-        data_array[index_M0][223:192] <= write_data[223:192];
-      end
-
-      if(write_word_enable[7] && darray_wen) begin
-        data_array[index_M0][255:224] <= write_data[255:224];
-      end
-
-      if(write_word_enable[8] && darray_wen) begin
-        data_array[index_M0][287:256] <= write_data[287:256];
-      end
-
-      if(write_word_enable[9] && darray_wen) begin
-        data_array[index_M0][319:288] <= write_data[319:288];
-      end
-
-      if(write_word_enable[10] && darray_wen) begin
-        data_array[index_M0][351:320] <= write_data[351:320];
-      end
-
-      if(write_word_enable[11] && darray_wen) begin
-        data_array[index_M0][383:352] <= write_data[383:352];
-      end
-
-      if(write_word_enable[12] && darray_wen) begin
-        data_array[index_M0][415:384] <= write_data[415:384];
-      end
-
-      if(write_word_enable[13] && darray_wen) begin
-        data_array[index_M0][447:416] <= write_data[447:416];
-      end
-
-      if(write_word_enable[14] && darray_wen) begin
-        data_array[index_M0][479:448] <= write_data[479:448];
-      end
-
-      if(write_word_enable[15] && darray_wen) begin
-        data_array[index_M0][511:480] <= write_data[511:480];
+      for (int i = 0; i < 16; i++) begin
+        if(write_word_enable[i] && darray_wen) begin 
+          data_array[index_M0][32 * i +: 32] <= write_data[32 * i +: 32];
+        end
       end
     end
   end
-
 
   // Select output word
   assign data_array_output = data_array[index_M0];
@@ -526,7 +366,6 @@ module lab3_cache_CacheBaseDpath
         .sel   (offset_M0),
         .out   (memresp_msg_data)
       );
-
 
 endmodule
 
